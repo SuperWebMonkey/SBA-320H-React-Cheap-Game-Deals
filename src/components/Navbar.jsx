29897ff88f7d5832248f5f941f6c9dae1e7bd494
SuperWebMonkey <vikingsattack@outlook.com>
2024-11-16 22:46:01 -0800

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,31 @@ function Navbar() {
   return (
     // <!-- Navbar -->
     <section id="navbar">
-      <div class="logo">
+      <div className="logo">
         <a href="#">
           <img id="logo-pic" src="/img/negative-man-pic.png" alt="Logo" />
         </a>
       </div>
-      <a href="./" class="page tech-shop">
+      <a href="./" className="page tech-shop">
         Digi World
       </a>
-      <a href="./" class="page home-page">
+      <a href="./" className="page home-page">
         Home
       </a>
-      <a href="./about" class="page about-page">
+      <a href="./about" className="page about-page">
         About
       </a>
-      <a href="./contact" class="page contact-page">
+      <a href="./contact" className="page contact-page">
         Contact
       </a>
-      <a href="./admin" class="page admin-page">
+      <a href="./admin" className="page admin-page">
         Admin
       </a>
-      <a href="#" class="page login" style="float: right">
+      <a href="#" className="page login" style="float: right">
         login
       </a>
-      <a href="#" class="page wishlist" style="float: right">
-        wishlist <i class="fas fa-heart"></i>
+      <a href="#" className="page wishlist" style="float: right">
+        wishlist <i className="fas fa-heart"></i>
       </a>
     </section>
   );
