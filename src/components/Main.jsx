5be0f01f4933a55d25e235d4c40623ea2fe69a17
SuperWebMonkey <vikingsattack@outlook.com>
2024-11-16 21:08:03 -0800

    start of digi search feature

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,10 @@
+/**
+ *
+ * Main component that contains the digimon items/cards
+ * api source: https://digimon-api.vercel.app/
+ *
+ */
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -6,12 +13,16 @@ const api_url = "https://digimon-api.vercel.app/api/digimon";
 function Main() {
   const [digimons, setDigimons] = useState([]);
   const [loadDigimons, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [filterDigi, setFilter] = useState([]);
 
+  // Use effect for getting the digi api
   useEffect(() => {
     axios
       .get(api_url)
       .then((res) => {
         setDigimons(res.data);
+        setFilter(res.data);
         setLoading(false);
       })
       .catch((e) => {
@@ -20,6 +31,19 @@ function Main() {
       });
   }, []);
 
+  // Search Function
+  const searchFeature = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearch(query);
+
+    const filteredList = digimons.filter((digi) => {
+      digi.name.toLowerCase().includes(query) ||
+        digi.level.toLowerCase().includes(query);
+    });
+
+    setFilter(filteredList);
+  };
+
   return (
     <div id="main-section">
       <h1 id="digimon-world-title">Digimon World</h1>
@@ -27,6 +51,14 @@ function Main() {
         Welcome to Digi World. A place where you can find and select your
         Digimon
       </p>
+
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="search for a digimon"
+        value={search}
+        onChange={searchFeature}
+      />
 
       {loadDigimons ? (
         <p>Loading all digimons...</p>
